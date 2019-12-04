bab933a9c19fcde05dbe2c5fa61150801076fa4d
Jeremiah Rodden <jrodden1@gmail.com>
2019-12-04 16:45:28 -0700

    added comments explaining some GO cli programs; also built executable using cli

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+//
 package main
 
 import "fmt"
@@ -5,3 +6,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 }
+
+// To run this, go to the terminal then run:  go run main.go
+// go run, compiles and immediately executes
+// go build, compiles, does not execute, but creates executable file.  (see "main" file in this folder)
